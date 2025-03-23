c1fe92824628ce727d5211619a16ff12e6a53e99
hotthings7 <hotthings7@tutamail.com>
2025-03-23 17:40:35 +0300

    add the files

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
-# app.py
-from flask import Flask, send_file
 import os
-import requests
-import time
-import random
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return send_file('index.html')
-while True:
-    try:
-        requests.get(f'https://investgition.onrender.com/?rand={random.randint(1000,9999)}')
-        time.sleep(300)  # Ping every 5 mins
-    except Exception as e:
-        print("Ping failed:", e)
+def home():
+    return "System Defender Interface"
 
-
-
-if __name__ == '__main__':
-    # Get PORT from Render environment variable or default to 5000
-    port = int(os.environ.get('PORT', 6000))
-    # Bind to 0.0.0.0 to be externally visible on Render
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
