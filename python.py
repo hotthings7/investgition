0af6c3ae4756dc0593d94a52da9c583917bc3535
hotthings7 <hotthings7@tutamail.com>
2025-03-23 16:01:47 +0300

    add the files

--- python.py
+++ python.py
@@ -1,3 +1,15 @@
+# app.py
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
 # keepalive.py
 import requests
 import time
